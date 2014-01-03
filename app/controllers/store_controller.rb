fde453d59dfc1bce961d5342b38131085ecd2b2e
zadeev <zadeev@gmail.com>
2014-01-03 13:42:43 +0400

    D_complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	count_index
+  end
+
+  def count_index
+  	if session[:counter].nil?
+  		session[:counter] = 1
+  	else
+  		session[:counter] += 1
+  	end
   end
 end
