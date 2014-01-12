f2bb471b44143faececa79369cda188d55137775
zadeev <zadeev@gmail.com>
2014-01-12 23:41:44 +0400

    f2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,6 +33,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_path }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
