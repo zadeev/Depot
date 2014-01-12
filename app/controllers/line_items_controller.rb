85276027a06f512f7c438c56b59bda5537e7b44d
zadeev <zadeev@gmail.com>
2014-01-13 00:02:34 +0400

    tryin_to_destroy

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -68,6 +68,7 @@ class LineItemsController < ApplicationController
     
     respond_to do |format|
       format.html { redirect_to :back }
+      format.js
       format.json { head :no_content }
     end
   end
