8230e075cd070951073169b0d9fb84c8c0e83e7e
zadeev <zadeev@gmail.com>
2014-01-19 17:11:22 +0400

    mid_g1

---
+++ app/models/order.rb
@@ -0,0 +1,15 @@
+class Order < ActiveRecord::Base
+	has_many :line_items, dependent: :destroy
+
+	validates :name, :address, :email, presence: true
+    #validates :pay_type, inclusion: PAYMENT_TYPES
+	PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      self.line_items << item #self or not self
+    end
+  end
+
+end
