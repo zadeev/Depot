bbda253c2cb825bdae5719f25e47e4c4b294adfd
zadeev <zadeev@gmail.com>
2014-01-25 17:49:54 +0400

    i1

---
+++ db/migrate/20140125123000_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    drop_table :users
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140116184915) do
+ActiveRecord::Schema.define(version: 20140125123000) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20140116184915) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
