b9510c88a8d3287283771a773290429380572c44
zadeev <zadeev@gmail.com>
2014-01-25 15:29:30 +0400

    g2_atom_ok

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -35,10 +35,10 @@ class LineItemsControllerTest < ActionController::TestCase
     end
   end
 
-  test "markup needed for store.js.coffee is in place" do get :index
-    assert_select '.store .entry > img', 3
-    assert_select '.entry input[type=submit]', 3
-  end
+#  test "markup needed for store.js.coffee is in place" do get :index
+#    assert_select '.store .entry > img', 3
+#    assert_select '.entry input[type=submit]', 3
+#  end
 
   test "should show line_item" do
     get :show, id: @line_item
